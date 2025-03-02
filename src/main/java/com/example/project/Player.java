f2d1c240ed2a04a3569b1b34a6bc63dbaac1a45a
rchen5884 <148903646+rchen5884@users.noreply.github.com>
2025-03-02 02:53:27 +0000

    poker

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -1,5 +1,6 @@
 package com.example.project;
 import java.util.ArrayList;
+import java.util.*;
 
 
 public class Player{
@@ -10,6 +11,7 @@ public class Player{
     
     public Player(){
         hand = new ArrayList<>();
+        allCards = new ArrayList<>();
     }
 
     public ArrayList<Card> getHand(){return hand;}
@@ -28,28 +30,51 @@ public class Player{
             allCards.add(c);
         }
 
-        allCards.sort(null);
-        System.out.print(allCards);
 
 
         return "Nothing";
     }
 
     public void sortAllCards() {
-        allCards.sort((card1, card2) -> Utility.getRankValue(card1.getRank()) - Utility.getRankValue(card2.getRank()));
+        for (int i = 0; i < allCards.size() - 1; i++) {
+            for (int j = i + 1; j < allCards.size(); j++) {
+                if (Utility.getRankValue(allCards.get(i).getRank()) > Utility.getRankValue(allCards.get(j).getRank())) {
+                    Card temp = allCards.get(i);
+                    allCards.set(i, allCards.get(j));
+                    allCards.set(j, temp);
+                }
+            }
+        }
     }
-    
-    
     
     
 
     public ArrayList<Integer> findRankingFrequency(){
-        
-        return new ArrayList<>(); 
+        ArrayList<Integer> frequency = new ArrayList<Integer>();
+        for (int i = 0; i < 13; i++) {
+            String rank = ranks[i];
+            frequency.add(0);
+            for (Card card : allCards) {
+                if (card.getRank().equals(rank)) {
+                    frequency.set(i, frequency.get(i) + 1);
+                }
+            }
+        }
+        return frequency;
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> frequency = new ArrayList<Integer>();
+        for (int i = 0; i < 4; i++) {
+            String suit = suits[i];
+            frequency.add(0);
+            for (Card card : allCards) {
+                if (card.getSuit().equals(suit)) {
+                    frequency.set(i, frequency.get(i) + 1);
+                }
+            }
+        }
+        return frequency; 
     }
 
    
